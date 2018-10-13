0a85e78e351f039cd5d40ddf8c92e3e511f0b3e9
Brian Thomas <Brianthomas571@gmail.com>
2018-10-13 00:01:18 -0500

    Updated html and started js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// 1. give user input to type in city name 
+// 2. user submits there city 
+// 3. brings city weather data back from OW api based on city value
+// 4. show user the city data in the html with icons
+// 5. clear the input value after submitted
+// 6. if input is empty and user submits then show user theres an error
+
+
+
+
+
+
+$(document).ready(function(){
+    const submitButton = document.querySelector(".submitButton");
+
+
+
+});
