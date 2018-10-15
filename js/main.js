b845b4f45a9719484e8038de8856a131cfc93fbf
Brian Thomas <brianthomas571@gmail.com>
2018-10-14 23:04:16 -0500

    updated data on html page with no styling

--- js/main.js
+++ js/main.js
@@ -8,32 +8,59 @@
 let URL = "http://api.openweathermap.org/data/2.5/weather?q=dallas&APPID=7307cd677e83e88b9129d98065459b1e";
 let key = "7307cd677e83e88b9129d98065459b1e";
    $(document).ready(function(){
+        $(".submitButton").click(function(){
+            let cityInput = $(".cityInput").val();
+            console.log(cityInput);
 
-    $(".submitButton").click(function(){
-        let cityInput = $(".cityInput").val();
-        console.log(cityInput);
-
-        $.ajax({
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${key}`,
-            type: "GET",
-            dataType:"json",
-            success: function(data){
-
-                weatherResults(data);
-                // clears input value
-                $(".cityInput").val("");
-            }
-
-
-        });
+            if(cityInput != ""){
+                $.ajax({
+                    url: `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${key}`,
+                    type: "GET",
+                    dataType:"json",
+                    success: function(data){
         
-    });
-
-
-    function weatherResults(data){
-        console.log(data);
-
-    }
-
+                        let results = weatherResults(data);
+        
+                        $(".showWeatherResults").html(results);
+                        // clears input value
+                        $(".cityInput").val("");
+                    }
+        
+        
+                });
+            } else {
+                $(".error").html("<div>Please enter a city</div>")
+                
+            }
+        });
+ 
    });
 
+
+   function weatherResults(data){
+    console.log(data);
+    return `
+                <h2>Weather Now for ${data.name}</h2>
+                <h3>Weather: ${data.weather[0].main}</h3>
+                <h3>Description: ${data.weather[0].description}</h3>
+                <h3>Temperature: ${data.main.temp} &deg;</h3>
+                <h3>Pressure: ${data.main.pressure} hpa</h3>
+                <h3>Humidity: ${data.main.humidity} %</h3>
+                <h3>Wind Speed: ${data.wind.speed} m/s</h3>
+                <h3>Wind Direction: ${data.wind.deg} &deg;</h3>
+            `;
+
+
+}
+
+
+//    "<h2 style='font-size:40px;font-weight:Bold;' class='text-center'>Current Weather for "+ data.name +"," + data.sys.country + "</h2>" + 
+//             "<h3 style='padding-left;40px'><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+//             "<h3 style='padding-left;40px'><strong>Description</strong>: <img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+ data.weather[0].description +"</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Temperature</strong>: "+ data.main.temp +"&deg;C</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Pressure</strong>: "+ data.main.pressure +" hPa</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Minimum Temperature</strong>: "+ data.main.temp_min +"&deg;C</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Maximum Temperature</strong>: "+ data.main.temp_max +"&deg;C</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Wind Speed</strong>: "+ data.wind.speed +" m/s</h3>" + 
+//             "<h3 style='padding-left;40px'><strong>Wind Direction</strong>: "+ data.wind.deg +"&deg;</h3>";
